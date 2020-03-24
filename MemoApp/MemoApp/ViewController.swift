97a8ea213dc69394cc4521f2263c7428ecadbad3
denny.k <denny.k@kakaocorp.com>
2020-03-25 00:04:45 +0900

    Memo VC Initialize

---
+++ MemoApp/MemoApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MemoApp
+//
+//  Created by 김태인 on 2020/03/24.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
