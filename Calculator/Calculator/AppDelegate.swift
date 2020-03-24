dcc67e35c740c455d36b83af8a3d003506aa3608
김태인 <denny@gimtaein-ui-MacBookPro.local>
2020-03-24 22:48:38 +0900

    initialize project

---
+++ Calculator/Calculator/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Calculator
+//
+//  Created by 김태인 on 2020/03/24.
+//  Copyright © 2020 김태인. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
+
